eb1144135843b44fca9e6df8d5c4bdc0e7a9bcdf
ehesp <elliot.hesp@gmail.com>
2020-11-19 12:55:11 +0000

    fix analyzer warnings

--- packages/firebase_storage/firebase_storage_web/lib/src/interop/storage_interop.dart
+++ packages/firebase_storage/firebase_storage_web/lib/src/interop/storage_interop.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: avoid_unused_constructor_parameters, non_constant_identifier_names
+// ignore_for_file: avoid_unused_constructor_parameters, non_constant_identifier_names, public_member_api_docs
 
 @JS('firebase.storage')
 library firebase.storage_interop;
